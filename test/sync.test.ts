724fdb3f8c443e270ff78ee894aac2f6417f7775
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2025-01-25 18:04:22 +0100

    test: add deno std expect

--- test/sync.test.ts
+++ test/sync.test.ts
@@ -1,7 +1,12 @@
 import assert from 'node:assert';
 import test from 'node:test';
+import { expect } from "@std/expect";
 
 test('synchronous passing test #unit', () => {
   // This test passes because it does not throw an exception.
   assert.strictEqual(1, 1);
 });
+
+test('expect deno usage', () => {
+  expect(1).toBeTruthy();
+})
